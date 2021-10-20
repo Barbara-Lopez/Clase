32e4781924d98e44f1be52a856bd5cc8d0609a29
Barbara <barbara.lopez@ikasle.egibide.org>
2021-10-20 09:59:54 +0200

    actualización

---
+++ php/EjerciciosInicio/E6.php
@@ -0,0 +1,17 @@
+
+<html>
+    <head>
+        <title>Ejercicio 6</title>
+    </head>
+    <body>
+        <?php 
+            $a=$_GET["a"];
+            $b=$_GET["b"];
+            $multiplicar=$a*$b;
+            
+            echo "<p>La multiplicación de $a y $b es $multiplicar</p>";
+            
+
+        ?>
+    </body>
+</html>
