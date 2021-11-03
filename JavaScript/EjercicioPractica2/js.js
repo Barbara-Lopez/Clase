0204967bacf87142e6df6f2576996e0a522b04d1
Bárbara <barbara.lopez@ikasle.egibide.org>
2021-11-03 18:46:52 +0100

    mejoras

--- JavaScript/EjercicioPractica2/js.js
+++ JavaScript/EjercicioPractica2/js.js
@@ -1,42 +1,56 @@
 window.onload = function() {
     let numeros=crearNumAleatorios();
 
-    alert(numeros.length+" "+numeros[0]+" "+numeros[1]);
-    
     for (i in numeros) {
-        alert(numeros[i]);
-        let boton=document.createElement("button");
-        let num=document.createTextNode(numero[i]);
-        boton.appendChild(num);
+        let boton=document.createElement("input");
+        attType=document.createAttribute("type");
+        attType.value="button";
+        boton.setAttributeNode(attType);
+        attValue=document.createAttribute("value");
+        attValue.value=numeros[i];
+        boton.setAttributeNode(attValue);
+        attName=document.createAttribute("name");
+        attName.value="tecladoNumerico";
+        boton.setAttributeNode(attName);
         document.body.appendChild(boton);
+        if(i==4){
+            let tabulador=document.createElement("br");
+            document.body.appendChild(tabulador);
+        }
     }
-
-
-
+    
 }
+
+function llenarClave(){
+    let botones=document.getElementsByName("tecladoNumerico");
+    for (i in botones) {
+        botones[i].addEventListener("click",ponerClave);
+    }
+}
+ 
+function ponerClave(){
+    let clave= this.value;
+    alert(clave);
+            
+};
 
 function crearNumAleatorios(){
     let numeros=[];
-
-    for(x=0;x<10;x++){
-        let num = Math.round(Math.random()*10);
-        repe="no";
-        if(numeros.leng>0){
-            for (y in numeros) {
-                if (y==num) {
-                    repe="si"; 
-                    break;
-                }
-            
-            }
+    cant=10;
+    while(numeros.length<cant){
+        let num = Math.round(Math.random()*(cant-1));
+        let repe="no";
+        for(x=0;x<numeros.length;x++){
+            if(numeros[x]==num){
+                repe="si"; 
+                break;
+            } 
         }
         if(repe=="no"){
-            numeros.push(num);
-        }
-        else{
-            x--;
+            numeros[x]=num;
         }
     }
+  
     return numeros;
 
 }
