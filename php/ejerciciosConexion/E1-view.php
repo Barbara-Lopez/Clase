870b477e082d8499c10d3cd0160897d01253378c
Barbara <barbara.lopez@ikasle.egibide.org>
2021-11-08 10:54:44 +0100

    conexión

---
+++ php/ejerciciosConexion/E1-view.php
@@ -0,0 +1,23 @@
+
+<html>
+    <head>
+        <title>Ejercicio 4</title>
+    </head>
+    <body>
+        <p>
+        </p>
+        <p>Solución:</p>
+        <h3>Lista de asistencia:</h3>
+        <ul>
+            <?php
+                añadirLista($boton,$nombre);
+            ?>
+        </ul>
+        <form action="E4.php" method="get" name="formulario">
+            <label for="nombre">Añadir asistente</label>
+            <input type="text" name="nombre" id="nombre">
+            <input type="submit" name="boton" value="Guardar">
+        </form>
+        <a href="E4.php?boton=vaciar">Vaciar lista</a>
+    </body>
+</html>
