0f084b325ad77eba1437d3f331fe438b493c1222
Bárbara <barbara.lopez@ikasle.egibide.org>
2021-11-11 17:52:28 +0100

    mejoras

--- JavaScript/EjercicioPractica2/js/datos.js
+++ JavaScript/EjercicioPractica2/js/datos.js
@@ -21,8 +21,9 @@ function MovimientoBancario(fecha,concepto,importe,saldo){
 function llenarDatos(){
     user= new User("11111111E",123456);
     dia =new Date(2000,2,12);
+    dia1 =new Date(2001,2,12);
     movimiento= new MovimientoBancario(dia,"Gasto casa",100,5000);
-    movimiento1=new MovimientoBancario(dia,"Ingreso casa",50,4950);
+    movimiento1=new MovimientoBancario(dia,"Ingreso casa",50,5050);
     movimiento2= new MovimientoBancario(dia,"Gasto coche",50,5000); 
     movimientosBancarios.push( movimiento,movimiento1,movimiento2);
     let persona= new Persona("Maria","Lopez",user,movimientosBancarios);
--- JavaScript/EjercicioPractica2/js/movimientos.js
+++ JavaScript/EjercicioPractica2/js/movimientos.js
@@ -8,4 +8,29 @@ personas=llenarDatos();
 function mostrarMovimientos(){
     let cookie=document.cookie.split('=');
     let nif=cookie[1];
+    buscarPersona(nif);
+}
+
+function buscarPersona(nif){
+    try {
+        persona=personas.find(p => p.user.nif == nif);
+        if (persona == undefined){           
+            throw "Persona no encontrada";
+        }else{
+            visualizarMovimientos(persona);
+        }
+    } 
+    catch (error) {
+        alert(error);
+    }
+}
+function visualizarMovimientos(persona){
+    let listaMovimientos=persona.movimientosBancarios;
+
+    listaMovimientos.sort();
+    lista=""
+    for (i in listaMovimientos.length) {
+        lista=lista+"La fecha es: "+listaMovimientos[i].fecha+" el concepto es: "+listaMovimientos[i].concepto+" el importe es: "+listaMovimientos[i].importe+" el saldo es: "+listaMovimientos[i].saldo+"\n"; 
+    }
+    alert(lista);
 }
