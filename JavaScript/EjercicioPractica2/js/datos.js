b40294305b530cf22eed0a20465ecafa41f52dd4
Bárbara <barbara.lopez@ikasle.egibide.org>
2021-11-11 18:25:20 +0100

    js

--- JavaScript/EjercicioPractica2/js/datos.js
+++ JavaScript/EjercicioPractica2/js/datos.js
@@ -24,7 +24,7 @@ function llenarDatos(){
     dia1 =new Date(2001,2,12);
     movimiento= new MovimientoBancario(dia,"Gasto casa",100,5000);
     movimiento1=new MovimientoBancario(dia,"Ingreso casa",50,5050);
-    movimiento2= new MovimientoBancario(dia,"Gasto coche",50,5000); 
+    movimiento2= new MovimientoBancario(dia1,"Gasto coche",50,5000); 
     movimientosBancarios.push( movimiento,movimiento1,movimiento2);
     let persona= new Persona("Maria","Lopez",user,movimientosBancarios);
     personas[0]=persona;
