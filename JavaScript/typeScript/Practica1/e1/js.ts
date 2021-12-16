6c98421d33fa096d8b430b599d26e5fa3419d837
Barbara <barbara.lopez@ikasle.egibide.org>
2021-12-16 14:04:48 +0100

    bootstrap

--- JavaScript/typeScript/Practica1/e1/js.ts
+++ JavaScript/typeScript/Practica1/e1/js.ts
@@ -5,45 +5,29 @@ $(document).ready(function() {
 function verificarDatos() {
     try {
         verificarNombre();
-        let edad:string=String($("#edad").val());
-        let sexo:string=String($("#sexo").val());
-        let peso:string=String($("#peso").val());
+        verificarEdad();
+        verificarSexo();
+        verificarPeso();
+        
+       
         let altura:string=String($("#altura").val());
         let estadoCivil:string=String($("#estadoCivil").val());
         
-        if(edad==""){
-            throw "La edad esta vacía"
-        }
-        if(sexo==""){
-            throw "El sexo esta vacío"
-        }
-        if(peso==""){
-            throw "El peso esta vacío"
-        }
+        
+        
+        
         if(altura==""){
             throw "La altura esta vacía"
         }
         if(estadoCivil==""){
             throw "El nombre esta vacío"
         }
-        var edadEx=RegExp("^[0-9]{1,2}$");
-        var sexoEx=RegExp("^[A-Z]{1}$");
        
-        if(!edadEx.test(edad)){
-            throw "La edad tiene que tener numeros"
-        }
-        if(!sexoEx.test(sexo)){
-            throw "El sexo tiene que ser una H de hombre o una M de mujer y estar en mayusculas"
-        }
-        if(peso==""){
-            throw "El peso esta vacío"
-        }
-        if(altura==""){
-            throw "La altura esta vacía"
-        }
-        if(estadoCivil==""){
-            throw "El nombre esta vacío"
-        }
+        
+       
+        
+        
+    
     } catch (error) {
         alert(error)
     }
@@ -51,7 +35,8 @@ function verificarDatos() {
 
 }
 
-function verificarNombre(){
+function verificarNombre()
+{
     let nombre:string=String($("#nombre").val());
     var nombreEx=RegExp("^[A-Za-z]{4,}$");
 
@@ -62,3 +47,40 @@ function verificarNombre(){
         throw "El nombre no tiene un minimo de 4 de longitud"
     }
 }
+function verificarEdad() {
+    let edad:string=String($("#edad").val());
+    var edadEx=RegExp("^[0-9]{1,2}$");
+
+    if(edad==""){
+        throw "La edad esta vacía"
+    }
+    if(!edadEx.test(edad)){
+        throw "La edad tiene que tener numeros"
+    }
+}
+
+function verificarSexo() {
+    let sexo:string=String($("#sexo").val());
+    var sexoEx=RegExp("^[A-Z]{1}$");
+
+    if(sexo==""){
+        throw "El sexo esta vacío"
+    }
+    if(!sexoEx.test(sexo)){
+        throw "El sexo tiene que ser una H de hombre o una M de mujer y estar en mayusculas"
+    }
+}
+
+function verificarPeso() {
+    let peso:string=String($("#peso").val());
+    var pesoEx=RegExp("^[0-9]{1,}$");
+    if(peso==""){
+        throw "El peso esta vacío"
+    }
+}
+
+
+
+
+
+
